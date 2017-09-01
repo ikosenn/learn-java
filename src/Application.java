e985a105994cbd80ebc1f2f204d61121a9a0924b
ikosenn <i@kosenn.me>
2017-08-31 23:41:46 -0700

    Play with variables

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,27 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		int myNumber = 99;  // 32
+		short myShort = 9999; // 16
+		long myLong = 4345; //  67
+		
+		double myDouble = 7.4745;
+		float myFloat = 459.8f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
 	}
 
 }
