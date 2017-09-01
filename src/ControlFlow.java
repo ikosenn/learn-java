f051eb71b4b21623db5f84ed6c8b5b6f055b4ea3
ikosenn <i@ikosenn.me>
2017-09-01 00:51:54 -0700

    Add if statements

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -12,6 +12,15 @@ public class ControlFlow {
 		for (int i=0; i<90; i++) {
 			System.out.println(i);
 		}
+		
+		int checker = -11;
+		if (checker > 10) {
+			System.out.println("checker greater than ten");
+		} else if (checker > 0 && checker <= 10)  {
+			System.out.println("checker is less than or equal to 10 but greater that zero");
+		} else {
+			System.out.println("checker is less than zero");
+		}
 	}
 
 }
