7bb1abba7d01566193317560b16ec931a6d3001b
ikosenn <i@ikosenn.me>
2017-09-03 23:09:41 -0700

    Create simple  classes

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -2,6 +2,10 @@
 class Person {
 	 String name;
 	 int age;
+	 void speak() {
+		System.out.println(
+			"My name is : " + this.name + " and my age is " + this.age); 
+	 }
 }
 
 public class JavaClass {
@@ -11,5 +15,6 @@ public class JavaClass {
 		person.age = 30;
 		System.out.println("Name: " + person.name);
 		System.out.println("Age: " + person.age);
+		person.speak();
 	}
 }
