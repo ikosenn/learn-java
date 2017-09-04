2f91ad334220151dccf0655a71309bfbf612330c
ikosenn <i@ikosenn.me>
2017-09-04 16:33:01 -0700

    Working with packages

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import ocean.Fish;
+import ocean.SeaWeed;
+import ocean.plants.Algae;
+
+public class App {
+	public static void main(String[] args) {
+		Fish fish = new Fish();
+		SeaWeed sea = new SeaWeed();
+		Algae alg = new Algae();
+	}
+}
